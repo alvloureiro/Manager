40f1d158b62a848fde063e4b80f867b2e7814e07
Andre Loureiro <loureiro.andrew@gmail.com>
2017-09-02 20:49:57 -0400

    [Employee] Add directory to employee components
    Adds a directory to hold all employee related components. Besides it
    adds a initial stub implementation to employee creator component.
    
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Router, Scene } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
-import EmployeeList from "./components/EmployeeList";
+import EmployeeList from "./components/employee/EmployeeList";
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
+      <Scene key="root" hideNavBar>
         <Scene key="auth">
           <Scene
             key="login"
             component={LoginForm}
             title="Login"
-            initial
           />
         </Scene>
         <Scene key="main">
---
+++ src/components/employee/EmployeeCreator.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const EmployeeCreator = () => {
+    return (
+        <View>
+            <Text>EmployeeCreator</Text>
+        </View>
+    );
+};
+
+export default EmployeeCreator;
