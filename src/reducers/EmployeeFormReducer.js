a2a5f6bca16a72e3203127d458e12bdceb607afb
Andre Loureiro <loureiro.andrew@gmail.com>
2017-09-04 19:12:18 -0400

    [Reducers] Add employee reducer
    Add employee reducer to our combine reducer function call.
    
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -11,6 +11,6 @@ export default (state = INITIAL_STATE, action) => {
     case EMPLOYEE_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value };
     default:
-      state;
+      return state;
   }
 };
